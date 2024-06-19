68b745ac8239fdca7980d26c80a45b8004a84cc6
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-19 14:20:50 +0530

    Added the hpp files to be include

---
+++ include/VehicleManagementSystem.hpp
@@ -0,0 +1,20 @@
+#ifndef VEHICLEMANAGEMENTSYSTEM_HPP
+#define VEHICLEMANAGEMENTSYSTEM_HPP
+
+#include <vector>
+#include <memory>
+#include "Vehicle.hpp"
+
+class VehicleManagementSystem {
+private:
+    std::vector<std::unique_ptr<Vehicle>> vehicles;
+
+public:
+    void addVehicle(std::unique_ptr<Vehicle> vehicle);
+    void removeVehicle(const std::string& brand, int year);
+    void displayVehicles() const;
+    Vehicle* searchVehicle(const std::string& brand) const;
+    void updateVehicle(const std::string& brand, int year);
+};
+
+#endif // VEHICLEMANAGEMENTSYSTEM_HPP
